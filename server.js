fb68b17ac401924a1cc52509e98938e8583e01a1
Odilia Lirani <oclirani@gmail.com>
2017-02-25 02:15:34 -0500

    server mod

--- server.js
+++ server.js
@@ -9,12 +9,14 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.sendFile('index.html', pathName);
 });
-app.get('/:file', (req, res) => {
-    const file = req.params.file;
-	if(file.indexOf('.json') !== -1) {
-		res.status(403).send("You can't access this file!");
-	}
-	res.sendFile(file, options);
+app.get('/:path', (req, res) => {
+    const path = req.params.path;
+	if (path == "joinus") {
+        res.sendFile('joinus.html', pathName);
+    } else {
+        res.status(404).send("NOT FOUND");
+    }
+	
 });
 app.listen(PORT, () => {
     console.log("Listening on port: " + PORT);
