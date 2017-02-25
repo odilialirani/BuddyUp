74168db67e16a849ea43730009e38c9977c6b4da
Odilia Lirani <oclirani@gmail.com>
2017-02-25 02:10:43 -0500

    added route

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.sendFile('index.html', pathName);
 });
-
+app.get('/:file', (req, res) => {
+    const file = req.params.file;
+	if(file.indexOf('.json') !== -1) {
+		res.status(403).send("You can't access this file!");
+	}
+	res.sendFile(file, options);
+});
 app.listen(PORT, () => {
     console.log("Listening on port: " + PORT);
 });
